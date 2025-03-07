fdebe6331b07a421b34cd21efa208f283f6be629
sehee0207 <lolry0207@gmail.com>
2025-03-08 00:45:10 +0900

    feat: router 추가

---
+++ src/app/develop/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DevelopPage = () => {
+  return <div>DevelopPage</div>;
+};
+
+export default DevelopPage;
---
+++ src/app/pm/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PmPage = () => {
+  return <div>PmPage</div>;
+};
+
+export default PmPage;
