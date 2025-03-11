4afae236bb2be29cac65b82a7ce6a3b9210da53b
sehee0207 <lolry0207@gmail.com>
2025-03-11 23:40:29 +0900

    feat: github pages 연결

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://sm-solux.github.io"
+      : "",
 };
 
 export default nextConfig;
